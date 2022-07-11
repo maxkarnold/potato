a9852627ea86866b6ef684b0afbd5aab26a51c49
Max Arnold <max.arnold@epsilon.com>
2022-07-10 22:54:39 -0500

    moving to angular

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,36 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  screenWidth: any;
+  @HostListener('window:resize', ['$event'])
+  onResize(event?: any) {
+    this.screenWidth = window.innerWidth;
+    if (this.screenWidth > 800) {
+      this.isHidden = false;
+      this.isMobile = false;
+    } else {
+      this.isHidden = true;
+      this.isMobile = true;
+    }
+  }
+
+  isMobile = true;
+  isHidden = true;
+  constructor() {
+    this.onResize();
+   }
+
+  ngOnInit(): void {
+  }
+
+  toggleNav() {
+    if (this.isMobile) {
+      this.isHidden = this.isHidden ? false : true; 
+    }
+  }
+}
