b127ca23b88d922130a029e3f6b7e6569ea0950d
Max Arnold <max.arnold@epsilon.com>
2022-08-13 16:14:07 -0500

    add dynamic age to bio

--- src/app/pages/bio/bio.component.ts
+++ src/app/pages/bio/bio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as dayjs from 'dayjs';
 
 @Component({
   selector: 'app-bio',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bio.component.scss']
 })
 export class BioComponent implements OnInit {
-
+  age = '';
   constructor() { }
 
   ngOnInit(): void {
+    this.calcAge()
+    console.log(this.age)
+  }
+
+  calcAge() {
+    const diff = Math.floor(dayjs().diff('2019-08-04', 'month', true));
+    this.age = `${Math.floor(diff / 12)} years${diff % 12 !== 0 ? ` and ${diff % 12} ${diff % 12 === 1 ? 'month': 'months'}`: ''}`;
   }
 
 }
