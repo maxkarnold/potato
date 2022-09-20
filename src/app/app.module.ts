59b4040be0946de848cc6d7fea24085f3778b44c
maxkarnold <60246034+maxkarnold@users.noreply.github.com>
2022-09-19 22:24:12 -0500

    refactor: :art: move components to shared folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SectionComponent } from './section/section.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SectionComponent } from './shared/section/section.component';
 import { BioComponent } from './pages/bio/bio.component';
 import { GalleryComponent } from './pages/gallery/gallery.component';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './shared/footer/footer.component';
 import { LogComponent } from './pages/log/log.component';
 import { InstructionsComponent } from './pages/instructions/instructions.component';
 
@@ -26,12 +26,8 @@ import { TooltipModule } from '@cloudfactorydk/ng2-tooltip-directive';
     LogComponent,
     InstructionsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    TooltipModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, TooltipModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
